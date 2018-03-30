5dc735382429006a33501e852246cd1e3eb4522a
Ben Coveney <bencoveney@gmail.com>
2018-03-30 18:08:07 +0100

    resolved build issues

--- webpack.common.ts
+++ webpack.common.ts
@@ -26,6 +26,7 @@ export const configuration: Webpack.Configuration = {
   module: {
     rules: [
       {
+        exclude: resolvePath("node_modules"),
         include: resolvePath("src"),
         loaders: [ "awesome-typescript-loader" ],
         test: /\.tsx?$/,
@@ -37,6 +38,7 @@ export const configuration: Webpack.Configuration = {
       },
       {
         enforce: "pre",
+        exclude: resolvePath("node_modules"),
         include: resolvePath("src"),
         loader: "tslint-loader",
         options: {
