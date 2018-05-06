162b58a6ef1a8d9ad169945b3b0423668513491d
Ben Coveney <bencoveney@gmail.com>
2018-05-06 21:07:21 +0100

    local server script

--- server/index.ts
+++ server/index.ts
@@ -1,27 +1,47 @@
 import * as Express from "express";
+import * as Path from "path";
 import * as Webpack from "webpack";
 import * as WebpackDevMiddleware from "webpack-dev-middleware";
 import * as WebpackHotMiddleware from "webpack-hot-middleware";
-
 import Config from "../webpack.dev";
+
 const compiler = Webpack(Config);
 
-// Tell express to use the webpack-dev-middleware and use the webpack.config.js
-// configuration file as a base.
+const resolvePath = (path: string) => Path.resolve(__dirname, path);
+
 const app = Express();
-app.use(
-  WebpackDevMiddleware(
-    compiler,
-    {
-      publicPath: (Config.output as { publicPath: string }).publicPath,
-    },
-  ),
-);
-app.use(WebpackHotMiddleware(compiler));
+
+if (process.env.NODE_ENV === "development") {
+  app.use(
+    WebpackDevMiddleware(
+      compiler,
+      {
+        publicPath: (Config.output as { publicPath: string }).publicPath,
+        stats: Config.stats,
+      },
+    ),
+  );
+  app.use(WebpackHotMiddleware(compiler));
+} else {
+  app.use(Express.static(resolvePath("../docs")));
+  app.get(
+    "*",
+    (request, response) => response.sendFile(resolvePath("../docs/index.html")),
+  );
+}
+
+const port = process.env.PORT || 8080;
 
 // Serve the files on port 3000.
 app.listen(
-  3000,
-  // tslint:disable-next-line:no-console
-  () => console.log("Example app listening on port 3000!\n"),
+  port,
+  (error: any) => {
+    if (error) {
+      // tslint:disable-next-line:no-console
+      console.error(error);
+    }
+
+    // tslint:disable-next-line:no-console
+    console.log(`Express listening on port ${port}\n`);
+  },
 );
